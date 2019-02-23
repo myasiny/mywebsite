4eeff1efc3bbc8aeb66b7d6c92ae765e3981923a
M Yasin Yıldırım <myasiny@gmail.com>
2019-02-23 23:55:06 +0300

    Splash Screen

--- myblog/views.py
+++ myblog/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from myblog.models import Post
 
 
+def splash(request):
+    return render(request, "splash.html")
+
+
 def index(request):
     post = Post.objects.order_by('date')
     return render(request, "index.html", {'result': post})
@@ -13,5 +17,5 @@ def detail(request, post_title):
     try:
         post = Post.objects.get(title=post_title)
     except Post.DoesNotExist:
-        raise Http404("Üzgünüm, böyle bir paylaşım bulunamadı...")
+        raise Http404("Sorry :(")
     return render(request, "detail.html", {'result': post})
