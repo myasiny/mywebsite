2e0cf28d6ac1da6e0651eb60343a32b761970459
M Yasin Yıldırım <myasiny@gmail.com>
2019-02-24 17:06:01 +0300

    Production Mode

--- untitled/settings.py
+++ untitled/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'None'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # TODO
 
 
 # Application definition
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, 'static')
 )
