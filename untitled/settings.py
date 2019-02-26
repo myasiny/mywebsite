6f97932266b7c405f74fca081754cacd507d1a86
M Yasin Yıldırım <myasiny@gmail.com>
2019-02-26 23:37:23 +0300

    About Page

--- untitled/settings.py
+++ untitled/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'None'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # TODO
 
@@ -122,10 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
+
+# STATIC_ROOT = (
+#     os.path.join(BASE_DIR, 'static')
+# )
