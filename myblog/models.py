fcd2874a6f9c06a0c093dcbf0f076737e11de491
M. Yasin Yıldırım <yildirim.muhammed@turkcell.com.tr>
2020-02-08 16:39:50 +0300

    New Design

--- myblog/models.py
+++ myblog/models.py
@@ -1,12 +1,12 @@
-from datetime import datetime
-
 from django.db import models
+
+from datetime import datetime
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField(blank=True, null=True)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    title = models.CharField(blank=False, max_length=100)
+    body = models.TextField(blank=False)
+    date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.title
